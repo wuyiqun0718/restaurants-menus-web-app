30ee15e3125ba60f49982259b2b6ed0ed90fd2a6
wuyiqun0718 <wuyiqun0718@gmail.com>
2015-10-01 18:49:02 -0500

    add new item create form request and redirect

--- project.py
+++ project.py
@@ -20,20 +20,26 @@ def restaurantMenu(restaurant_id):
 	return render_template('menu.html', restaurant=restaurant, items = items)
 
 # Task 1: Create route for newMenuItem function here
-@app.route('/restaurants/<int:restaurant_id>/new')
+@app.route('/restaurants/<int:restaurant_id>/new', methods=['GET','POST'])
 def newMenuItem(restaurant_id):
-    return "page to create a new menu item. Task 1 complete!"
+    if request.method == 'POST':
+    	newItem = MenuItem(name=request.form['name'], restaurant_id=restaurant_id)
+    	session.add(newItem)
+    	session.commit()
+    	return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+    	return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 
 # Task 2: Create route for editMenuItem function here
 
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit')
-def editMenuItem(restaurant_id, menu_id):
+def edit(restaurant_id, menu_id):
     return "page to edit a menu item. Task 2 complete!"
 
 # Task 3: Create a route for deleteMenuItem function here
 
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete')
-def deleteMenuItem(restaurant_id, menu_id):
+def delete(restaurant_id, menu_id):
     return "page to delete a menu item. Task 3 complete!"
 
 if __name__ == '__main__':
