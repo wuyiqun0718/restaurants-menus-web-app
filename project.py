f7305a81073269b24dd5598f3068dba556f593fa
wuyiqun0718 <wuyiqun0718@gmail.com>
2015-09-30 22:05:30 -0500

    Display the first restaurant's menu item's name

--- project.py
+++ project.py
@@ -1,10 +1,27 @@
-from flask import flask
+from flask import Flask
 app = Flask(__name__)
+
+# import CRUD Operations from Lesson 1
+from database_setup import Base, Restaurant, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# Create session and connect to DB
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
 @app.route('/hello')
 def HelloWorld():
-	return "Hello World"
+	restaurant = session.query(Restaurant).first()
+	items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+	output = ''
+	for i in items:
+		output += i.name
+		output += '</br>'
+	return output
 
 if __name__ == '__main__':
 	app.debug = True
